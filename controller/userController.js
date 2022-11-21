76e18c7347d224f0bdd77f6cb8aa1caa2f8ec240
Ashwani Yadav <ashwaniyadav6779@gmail.com>
2022-11-21 15:58:04 +0530

    second commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,5 @@
 const user = require("../model/userModel")
 const bcrypt = require("bcryptjs")
-// const { check, oneOf, validationResult } = require('express-validator');
 const validate = require("../middleware/validator")
 
 
@@ -10,15 +9,12 @@ const register =  async(req,res)=>{
   try{
     const {username, password, confirm, email,firstName, lastName} = req.body;
 
-  
-
   // confirm password
   if(password !== confirm){
     res.status(401).json({
       status:"Password not matched"
     })
   } else{
-    
     const User = await user.create({
       username,password,email,firstName,lastName
     })
@@ -29,22 +25,13 @@ const register =  async(req,res)=>{
         User.password =hash;
        await User.save()
 
-
        res.status(201).json({
         status:"success",
         data:User
       })
       })
-      
     })
- 
-   
-
   }
-
-
-
- 
   }catch(error){
     if(error.username){
       res.status(401).send({ result: "Fail", message: "User Name is already in use" })
@@ -62,7 +49,29 @@ const register =  async(req,res)=>{
   }
 }
 
-module.exports =  {register}
+
+
+// APi for login
+const login = async(req,res)=>{
+  try {
+    var data = await user.findOne({ username: req.body.username })
+    if (data) {
+        if (await bcrypt.compare(req.body.password, data.password))
+            res.send({ result: "Login sucessfully", acccess_token: data._id })
+        else
+            res.status(404).send({ result: "Fail", message: "Invalid Username or Password" })
+
+    }
+    else
+        res.status(404).send({ result: "Fail", message: "Invalid Username or Password" })
+        console.log("id",data._id)
+}
+catch (error) {
+    res.status(500).send({ result: "Fail", message: "Internal Server Error" })
+}
+}
+
+module.exports =  {register, login}
 
 
 
