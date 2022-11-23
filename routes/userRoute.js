705726bf1abfe14824a0554d2d570d674d4dac4d
Ashwani Yadav <ashwaniyadav6779@gmail.com>
2022-11-23 11:11:59 +0530

    fifth_commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const deleteUser = require("../controller/deleteController");
+const getAllUser = require("../controller/getAllUserController");
 const getUser = require("../controller/getUser");
 const login = require("../controller/loginController");
 const register = require("../controller/registerController");
@@ -16,7 +17,7 @@ router.post("/register",userValidationRules(), validate,register);
 router.post("/login",login);
 router.get("/get",isAuthenticatedUser,getUser);
 router.delete("/delete",isAuthenticatedUser,deleteUser)
-
+router.get("/list", getAllUser)
 
 
 module.exports=router
